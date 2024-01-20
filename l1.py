670a2cfa91f0987dfe1bc5c31bfb68575197b3c4
romapota <154024023+romapota@users.noreply.github.com>
2024-01-20 18:54:37 +0300

    Add files via upload

---
+++ l1.py
@@ -0,0 +1,27 @@
+def req(strs): # функция переставляет знаки по одному по очереди и считает подходит данная расстановка знаков для достижения цели или нет, если да, то печатает строку и выхожит из рекурсия, если нет, вызывает сама себя дальше
+    count = strs.count(' ')
+    if count > 0:
+        req(strs.replace(' ', '+', 1))
+        req(strs.replace(' ', '-', 1))
+        return
+    if eval(strs) == target: #функция переобразующая строку с вычислениями и подсчитыващая результат
+        file = open('output.txt', 'w+')
+        file.write(strs + '=' + str(target))
+        file.close()
+        print(strs, '=', target)
+        exit()
+
+def get_data():
+    global strs, target
+    with open('input.txt', 'r', encoding='utf-8') as file:  # открытие файла и считывание с него данных
+        strs = file.readline()
+        numbers = list(map(int, strs.split()))
+        target = numbers.pop()
+        strs = strs[strs.find(' ') + 1:strs.rfind(' ')]
+def main():
+    get_data()
+    req(strs)
+
+
+if __name__ == '__main__':
+    main()
